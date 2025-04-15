aa024ae01a92c6516b2351fea55d9934c77a0553
Fernando Alves <lfalvespe@gmail.com>
2025-04-14 22:00:02 -0300

    vite + React Project created

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+
+      <h1 className='page-title'>Menu Hamburguer</h1>
+
+      <div className="card">
+
+      </div>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <footer>FOOTER</footer>
+
+    </>
+  )
+}
+
+export default App
